d57a8973c405177e3f640503505a798395fbedb5
Shay <shanney49@gmail.com>
2022-06-11 12:19:41 +0800

    project has been updated

--- lib/Screens/Splash.dart
+++ lib/Screens/Splash.dart
@@ -14,7 +14,7 @@ class _SplashState extends State<Splash> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 5), () {
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginForm()));
     });
   }
